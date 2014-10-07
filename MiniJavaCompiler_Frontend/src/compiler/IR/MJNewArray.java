a414b2bda21a19bf192ae52f182f0e519cfb1a0a
BlibberBlobber <Mortenvorborg@hotmail.com>
2014-10-07 16:03:25 +0200

    NewArray lavet (går ud fra det er int)

--- MiniJavaCompiler_Frontend/src/compiler/IR/MJNewArray.java
+++ MiniJavaCompiler_Frontend/src/compiler/IR/MJNewArray.java
@@ -3,7 +3,22 @@ package compiler.IR;
 import compiler.PrettyPrinter;
 
 public class MJNewArray extends MJNew {
+	
+	private MJExpression size;
+	
+	public MJNewArray(){}
+	
+	public MJNewArray(MJExpression size){
+		this.size = size;
+	}
+	
+	public MJExpression getSize(){
+		return size;
+	}
 
 	public void prettyPrint(PrettyPrinter prepri) {
+		prepri.print("new int [");
+		this.size.prettyPrint(prepri);
+		prepri.print("]");
 	}
 }
