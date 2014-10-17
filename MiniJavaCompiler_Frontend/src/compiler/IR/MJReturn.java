e1c1b400a3d6668e149787c765ac271ede3a54c5
DreamSpawn <westh_mikkel@hotmail.com>
2014-10-17 15:05:39 +0200

    Fjernet check for om expression er en NewObjectExpression (den tidligere NoExpression)

--- MiniJavaCompiler_Frontend/src/compiler/IR/MJReturn.java
+++ MiniJavaCompiler_Frontend/src/compiler/IR/MJReturn.java
@@ -17,7 +17,7 @@ public class MJReturn extends MJStatement {
 	public void prettyPrint(PrettyPrinter prepri) {
 		prepri.print("return");
 
-		if (!(this.retExp instanceof MJNewObjectExpression) && this.retExp != null) {
+		if (this.retExp != null) {
 			prepri.print(" ");
 			this.retExp.prettyPrint(prepri);
 		}
